9775e142aee9adbd1831a05ca830a061f4bbd9fd
leonid-efremov <efremov.leonid@outlook.com>
2022-03-30 17:50:05 +0300

    restructure and prepare

---
+++ Lang_gen.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 30 17:28:21 2022
+
+@author: Leonid_PC
+"""
+
+from random import choice
+
+
+def lang_preprocess(text, abc):
+    """
+    Create preprocessed data for model from text
+    """
+    words2 = dict()
+    words3 = dict()
+    
+    # remove all sybols which are not letters        
+    corpus = text.lower()  # convert to lowercase  
+    corpus = ''.join([i for i in corpus if i in abc])           
+    corpus = corpus.split()  # split text in single words
+
+    # find unique words and next words for each in bigramm model
+    for prev, curr in zip(corpus, corpus[1:]):
+        if prev not in words2.keys():
+            words2[prev] = []
+        words2[prev].append(curr)
+    # same for trigramm model    
+    for prev, curr, n in zip(corpus, corpus[1:], corpus[2:]):
+        if (prev, curr) not in words3.keys():
+            words3[(prev, curr)] = []
+        words3[(prev, curr)].append(n)
+            
+    # remove empty elements
+    words2 = dict([(k, v) for k,v in words2.items() if len(v) > 0])
+    words3 = dict([(k, v) for k,v in words3.items() if len(v) > 0])
+        
+    return corpus, words2, words3
+
+
+# Language n-gram models
+def bigramm_model(init_word, words2, num_words=15):
+    "Generate phrase in bigramm model"
+    
+    phrase = list()
+    words = [item for sublist in list(words2.values()) for item in sublist]
+    
+    # set initial word or pick random
+    if init_word:
+        init_word = init_word.lower()
+        init_word = init_word.split()
+        for i in init_word:
+            phrase.append(i) 
+    else:
+        phrase.append(choice(words))         
+    li = len(phrase)
+    
+    for _ in range(num_words - li):
+        # if word appear in train data pick random next word
+        if phrase[-1] in list(words2.keys()):
+            candidates = words2[phrase[-1]]
+            phrase.append(choice(candidates))
+        # else pick just random word
+        else:
+            phrase.append(choice(words))
+    
+    return phrase
+
+
+
+def trigramm_model(init_word, words2, words3, num_words=15):
+    "Generate phrase in trigramm model"
+    
+    phrase = list()
+    words = [item for sublist in list(words3.values()) for item in sublist]
+    
+    # set two initial words or pick random
+    if init_word:
+        init_word = init_word.lower()                  
+        init_word = init_word.split()
+        for i in init_word:
+            phrase.append(i)
+    else:
+        phrase.append(choice(words))  
+    # if word appear in train data pick random next word
+    if len(phrase) < 2:
+        if phrase[-1] in list(words2.keys()):
+            candidates = words2[phrase[-1]]
+            phrase.append(choice(candidates))
+        # else pick just random word
+        else:
+            phrase.append(choice(words))
+    li = len(phrase)
+        
+    for i in range(num_words - li):
+        # if word appear in train data pick random next word
+        if (phrase[-2], phrase[-1]) in list(words3.keys()):
+            candidates = words3[(phrase[-2], phrase[-1])]
+            phrase.append(choice(candidates))
+        # elif pick word from bigramm model
+        elif phrase[-1] in list(words2.keys()):  
+            candidates = words2[phrase[-1]]
+            phrase.append(choice(candidates))
+        # else pick just random word
+        else:
+            phrase.append(choice(words))
+    
+    return phrase
+
+    
--- Text_Generator.py
+++ Text_Generator.py
@@ -5,25 +5,27 @@ Created on Sat Dec 18 16:45:42 2021
 @author: Leonid_PC
 
 Генератор теста
+main файл:
+    - простая грамматическая модель
+    - интерфейс для вызова LSTM модели
+    - основная программа
 """
 
 import os 
 import json
 import pickle
-import pandas as pd
-from random import choice
-import torch
-from torch import nn
-import torch.nn.functional as F
+from Lang_gen import trigramm_model, lang_preprocess
 
 
 
 class TextGenerator:
 
-    def __init__(self):
+    def __init__(self, model_type='LSTM'):
         """
         Class for generating text
         """
+        
+        self.model_type = model_type
 
         self.train_text = None
         self.words2 = dict()  # collecting all words and next words
@@ -36,15 +38,8 @@ class TextGenerator:
         self.abc = ''.join([chr(i) for i in range(a, a + 32)] 
                                 + [' '] + ['\n'] + [chr(a + 33)])
         
-    def encode(self):
-        """
-        Encode text from string to integer
-        """
-        chars = list(set(self.train_text))
-        pass
-        
 
-    def fit(self, source='WarAndPeace1.txt'):
+    def fit_for_lang(self, source='WarAndPeace1.txt'):
         "Train model: for words in 'source' find next words"
 
         file_name, file_extension = os.path.splitext(source)
@@ -61,114 +56,14 @@ class TextGenerator:
         #with open('DontRename&PlaceInOneFolder', 'wb') as w:
         with open('words', 'wb') as w:  # save data to file
             pickle.dump(self.train_text, w)
+        
             
-        
-    def _preprocess(self, text):
-        """
-        Create preprocessed data for model from text
-        """
-        # remove all sybols which are not letters        
-        self.corpus = self.train_text.lower()  # convert to lowercase  
-        self.corpus = ''.join([i for i in self.corpus if i in self.abc])           
-        self.corpus = self.corpus.split()  # split text in single words
-
-        # find unique words and next words for each in bigramm model
-        for prev, curr in zip(self.corpus, self.corpus[1:]):
-            if prev not in self.words2.keys():
-                self.words2[prev] = []
-            self.words2[prev].append(curr)
-        # same for trigramm model    
-        for prev, curr, n in zip(self.corpus,self.corpus[1:],self.corpus[2:]):
-            if (prev, curr) not in self.words3.keys():
-                self.words3[(prev, curr)] = []
-            self.words3[(prev, curr)].append(n)
-            
-        # remove empty elements
-        self.words2 = dict([(k, v) for k,v in self.words2.items() if len(v)>0])
-        self.words3 = dict([(k, v) for k,v in self.words3.items() if len(v)>0])
-        
-        return self.corpus, self.words2, self.words3
-        
-        
-     
-    # Language n-gram models
-    def bigramm_model(self):
-        "Generate phrase in bigramm model"
-        
-        phrase = list()
-        words = [item for sublist in list(self.words2.values()) \
-                 for item in sublist]
-        
-        # set initial word or pick random
-        if self.init_word is not None:
-            self.init_word = self.init_word.lower()
-            self.init_word = self.init_word.split()
-            for i in self.init_word:
-                phrase.append(i) 
-        else:
-            phrase.append(choice(words))         
-        li = len(phrase)
-        
-        for _ in range(self.num_words - li):
-            # if word appear in train data pick random next word
-            if phrase[-1] in list(self.words2.keys()):
-                candidates = self.words2[phrase[-1]]
-                phrase.append(choice(candidates))
-            # else pick just random word
-            else:
-                phrase.append(choice(words))
-        
-        return phrase
-    
-
-
-    def trigramm_model(self):
-        "Generate phrase in trigramm model"
-        
-        phrase = list()
-        words = [item for sublist in list(self.words3.values()) \
-                 for item in sublist]
-        
-        # set two initial words or pick random
-        if self.init_word is not None:
-            self.init_word = self.init_word.lower()                  
-            self.init_word = self.init_word.split()
-            for i in self.init_word:
-                phrase.append(i)
-        else:
-            phrase.append(choice(words))  
-        # if word appear in train data pick random next word
-        if len(phrase) < 2:
-            if phrase[-1] in list(self.words2.keys()):
-                candidates = self.words2[phrase[-1]]
-                phrase.append(choice(candidates))
-            # else pick just random word
-            else:
-                phrase.append(choice(words))
-        li = len(phrase)
-            
-        for i in range(self.num_words - li):
-            # if word appear in train data pick random next word
-            if (phrase[-2], phrase[-1]) in list(self.words3.keys()):
-                candidates = self.words3[(phrase[-2], phrase[-1])]
-                phrase.append(choice(candidates))
-            # elif pick word from bigramm model
-            elif phrase[-1] in list(self.words2.keys()):  
-                candidates = self.words2[phrase[-1]]
-                phrase.append(choice(candidates))
-            # else pick just random word
-            else:
-                phrase.append(choice(words))
-        
-        return phrase
-    
-        
-        
-    def generate(self, model='trigram'):
+    def generate(self):
         "Generation of phrase for initial word"
         
-        if model == 'trigram':
-            phrase = self.trigramm_model()        
+        if self.model_type == 'Lang':
+            phrase = trigramm_model(self.init_word, self.words2, self.words3, 
+                                    num_words=self.num_words)        
         res = ' '.join(phrase)  # save and print resulting phrase
         print(res)
         return res
@@ -183,37 +78,32 @@ class TextGenerator:
         If data is None load saved file
         """
         
-        self.words2 = dict()
-        self.words3 = dict()
-        
-        if data is not None:  # train model if necessary data specified
-            for i in data:
-                self.fit(source=i)
+        if data is not None:  
+            # train model if necessary data specified
+            if self.model_type == 'Lang':
+                for i in data:
+                    self.fit_for_lang(source=i)
         else:
-            # DontRename&PlaceInOneFolder
-            with open('DontRename&PlaceInOneFolder', 'rb') as w:  # load data
-                self.train_text = pickle.load(w)
+            # load data
+            if self.model_type == 'Lang':
+                # DontRename&PlaceInOneFolder
+                with open('DontRename&PlaceInOneFolder', 'rb') as w:
+                    self.train_text = pickle.load(w)
         
-        self.corpus, self.words2, self.words3 = \
-            self._preprocess(self.train_text)
-        
-        
-
-def main():
-    "Main function"
+        self.corpus, self.words2, self.words3 = lang_preprocess(self.train_text,
+                                                                self.abc) 
     
-    gen = TextGenerator()  # initate model
+    
+                   
+if __name__ == '__main__':  
+    
+    gen = TextGenerator(model_type='Lang')  # initate model
     
     train_texts = ['WarAndPeace1.txt', 'HarryPotter.txt']  # ['result.json']
     gen.prepare_model(data=train_texts)  # data=train_texts   
     #start_word = str(input('Enter your word:'))  # generate phrase from word
     #gen.init_word = 'утро доброе'
     p = gen.generate()    
-    return gen, p
-                   
-if __name__ == '__main__':    
-    gen, p = main()
-
     
     
     
